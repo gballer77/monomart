36f0c045f02159170ce8654cd4dcf2eea7ae5bb0
Bryan Neva <bneva@pivotal.io>
2020-01-15 10:22:42 -0500

    Move pivmart artifact into subdirectory. Add product-api directory with clean spring boot

---
+++ product-api/src/main/java/io/pivotal/productapi/ProductApiApplication.java
@@ -0,0 +1,13 @@
+package io.pivotal.productapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiApplication.class, args);
+	}
+
+}
