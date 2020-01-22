4df8d274de7450eaddb04cb3d7aa55414b501213
Daniel Frey <dfrey@pivotal.io>
2020-01-22 10:13:31 -0500

    remove hard coded paths in contract tests

---
+++ pivmart/src/test/java/io/pivotal/pivmart/products/AbstractFakeNexusSetup.java
@@ -0,0 +1,35 @@
+package io.pivotal.pivmart.products;
+
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.cloud.contract.stubrunner.junit.StubRunnerExtension;
+
+import java.io.File;
+
+import static org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties.StubsMode.REMOTE;
+
+public abstract class AbstractFakeNexusSetup {
+
+    @RegisterExtension
+    static StubRunnerExtension stubRunnerExtension = new StubRunnerExtension()
+            .stubsMode(REMOTE)
+            .repoRoot(repoRoot())
+            .downloadStub("io.pivotal:product-api").withPort(8081)
+            .withMappingsOutputFolder("build/outputmappings");
+
+
+    protected static String repoRoot() {
+        try {
+            String userPath = System.getProperty("user.dir");
+            userPath = userPath.substring( 0, userPath.lastIndexOf('/'));
+            userPath += "/nexus";
+            File dir = new File(userPath);
+            return "stubs://file://" + dir.toString();
+        }
+        catch (Exception e) {
+            System.err.println(e);
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+}
--- pivmart/src/test/java/io/pivotal/pivmart/products/CatalogClientContractTests.java
+++ pivmart/src/test/java/io/pivotal/pivmart/products/CatalogClientContractTests.java
@@ -4,29 +4,17 @@ import io.pivotal.pivmart.models.Catalog;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
-import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK;
 
-// TODO: need to reference nexus relatively
-
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@AutoConfigureStubRunner(
-        stubsMode = StubRunnerProperties.StubsMode.REMOTE,
-        ids = "io.pivotal:product-api:+:8081",
-//        repositoryRoot = "stubs://file://../nexus/META-INF",
-        repositoryRoot = "stubs://file://Users/danielfrey/Development/git/bryanneva/pivmart/nexus/META-INF",
-        properties="stubs.find-producer=true"
-)
-@AutoConfigureJsonTesters
-public class CatalogClientContractTests {
+@ExtendWith({SpringExtension.class})
+@SpringBootTest(webEnvironment = MOCK)
+public class CatalogClientContractTests extends AbstractFakeNexusSetup {
 
     @Autowired
     private CatalogClient subject;
