2eb7213ced572ed8484cabfacb7f34ca8847546d
mkirl <michael.e.kirl@gmail.com>
2024-02-07 13:24:24 -0600

    add mock to purchase service

--- src/test/java/mart/mono/purchases/PurchasesTest.java
+++ src/test/java/mart/mono/purchases/PurchasesTest.java
@@ -1,12 +1,17 @@
 package mart.mono.purchases;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static java.util.Collections.singletonList;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasKey;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -18,10 +23,16 @@ class PurchasesTest {
     @Autowired
     MockMvc mockMvc;
 
+    @MockBean
+    PurchasesService mockPurchaseService;
+
     @Test
     void purchases_list() throws Exception {
+        when(this.mockPurchaseService.getAll()).thenReturn(singletonList(Purchase.builder().build()));
+
         mockMvc.perform(get("/api/purchases"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.length()", greaterThan(0)));
+                .andExpect(jsonPath("$.length()", greaterThan(0)))
+                .andExpect(jsonPath("$[0]", hasKey("items")));
     }
 }
