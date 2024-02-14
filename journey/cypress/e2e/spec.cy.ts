ad37f974c25bee8bbc702260678dd39a69d16b02
Vitaliy Solomonov <vitaliy.y.solomonov.mil@swf.army.mil>
2024-02-14 14:21:14 -0600

    e2e complete

--- journey/cypress/e2e/spec.cy.ts
+++ journey/cypress/e2e/spec.cy.ts
@@ -10,11 +10,21 @@ describe('monomart app', () => {
     })
   })
 
-  it('shows the selected product in the cart', () => {
+  it('should show the selected product in the cart', () => {
     cy.get('span').click()
-    cy.get('.cart-item-list').within(()=>{
-      cy.findByRole('listItem', {name: /coffee mug/i})
+    cy.findByRole('heading', {name: /cart/i})
+    cy.get('ul > :nth-child(1)').within(() => {
+      cy.findByText('Coffee Mug (10.99)')
     })
+  })
 
+  it('should checkout the product from the shopping cart', () => {
+    cy.get('span').click()
+    cy.findByRole('button',{name: 'Checkout (10.99)'}).click()
+    cy.wait(5000)
+    cy.findByRole('button',{name: 'Checkout (0.00)'})
+    cy.get(':nth-child(1) > main > .quantity').within(() => {
+      cy.findByText('Qty: 10') // need to decrease Qty in implementation
+    })
   })
 })
